a9d3013b52ef86b05db83a8c7ab1c823cd7f989c
Codorono <codorono@live.com>
2023-03-23 11:51:44 -0700

    Updated.

---
+++ Get-ScreenBuffer.ps1
@@ -0,0 +1,38 @@
+﻿#===================================================================================================
+
+Set-StrictMode -Version Latest
+
+#===================================================================================================
+
+# get screen buffer rectangle
+
+$Width = $Host.UI.RawUI.BufferSize.Width
+$Height = $Host.UI.RawUI.CursorPosition.Y
+
+$Rectangle = New-Object "System.Management.Automation.Host.Rectangle" 0, 0, $Width, $Height
+
+# get screen buffer contents
+
+$Buffer = $Host.UI.RawUI.GetBufferContents($Rectangle)
+
+# iterate through screen buffer lines
+
+$Line = New-Object "System.Text.StringBuilder" $Width
+
+for ($Row = 0; $Row -lt $Height; $Row++)
+{
+    # concatenate line chars
+
+    [void] $Line.Clear()
+
+    for ($Col = 0; $Col -lt $Width; $Col++)
+    {
+        [void] $Line.Append(($Buffer[$Row, $Col]).Character)
+    }
+
+    # output line
+
+    Write-Output $Line.ToString().TrimEnd()
+}
+
+#===================================================================================================
