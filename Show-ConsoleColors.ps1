b878f0270efb6f01dd634043ff20aa6a9d9531d4
Codorono <codorono@live.com>
2023-04-14 19:13:12 -0700

    Updated.

--- Show-ConsoleColors.ps1
+++ Show-ConsoleColors.ps1
@@ -42,12 +42,12 @@ Write-Host ("{0}  DefaultColor      - {1};{2}" -f $DefaultEscSeq, $DefaultForegr
 $FormatAccentCode = $ForegroundCodes[[int] $Host.PrivateData.FormatAccentColor]
 Write-Host ("`e[{0}m  FormatAccentColor - {0}{1}" -f $FormatAccentCode, $DefaultEscSeq)
 
+$ErrorAccentCode = $ForegroundCodes[[int] $Host.PrivateData.ErrorAccentColor]
+Write-Host ("`e[{0}m  ErrorAccentColor  - {0}{1}" -f $ErrorAccentCode, $DefaultEscSeq)
+
 $ErrorForegroundCode = $ForegroundCodes[[int] $Host.PrivateData.ErrorForegroundColor]
 $ErrorBackgroundCode = $BackgroundCodes[[int] $Host.PrivateData.ErrorBackgroundColor]
 Write-Host ("`e[{0};{1}m  ErrorColor        - {0};{1}{2}" -f $ErrorForegroundCode, $ErrorBackgroundCode, $DefaultEscSeq)
-
-$ErrorAccentCode = $ForegroundCodes[[int] $Host.PrivateData.ErrorAccentColor]
-Write-Host ("`e[{0};{1}m  ErrorAccentColor  - {0}{2}" -f $ErrorAccentCode, $ErrorBackgroundCode, $DefaultEscSeq)
 
 $WarningForegroundCode = $ForegroundCodes[[int] $Host.PrivateData.WarningForegroundColor]
 $WarningBackgroundCode = $BackgroundCodes[[int] $Host.PrivateData.WarningBackgroundColor]
@@ -70,48 +70,57 @@ Write-Host "PSReadLine Colors"
 $PSConsoleReadLineOptions = Get-PSReadLineOption
 
 $CommandEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.CommandColor
-Write-Host ("`e[{0}m  CommandColor            - {0}{1}" -f $CommandEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  CommandColor                - {0}{1}" -f $CommandEscSeq, $DefaultEscSeq)
 
 $CommentEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.CommentColor
-Write-Host ("`e[{0}m  CommentColor            - {0}{1}" -f $CommentEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  CommentColor                - {0}{1}" -f $CommentEscSeq, $DefaultEscSeq)
 
 $ContinuationPromptEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.ContinuationPromptColor
-Write-Host ("`e[{0}m  ContinuationPromptColor - {0}{1}" -f $ContinuationPromptEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  ContinuationPromptColor     - {0}{1}" -f $ContinuationPromptEscSeq, $DefaultEscSeq)
 
 $DefaultTokenEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.DefaultTokenColor
-Write-Host ("`e[{0}m  DefaultTokenColor       - {0}{1}" -f $DefaultTokenEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  DefaultTokenColor           - {0}{1}" -f $DefaultTokenEscSeq, $DefaultEscSeq)
 
 $EmphasisEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.EmphasisColor
-Write-Host ("`e[{0}m  EmphasisColor           - {0}{1}" -f $EmphasisEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  EmphasisColor               - {0}{1}" -f $EmphasisEscSeq, $DefaultEscSeq)
 
 $ErrorEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.ErrorColor
-Write-Host ("`e[{0}m  ErrorColor              - {0}{1}" -f $ErrorEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  ErrorColor                  - {0}{1}" -f $ErrorEscSeq, $DefaultEscSeq)
+
+$ErrorEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.InlinePredictionColor
+Write-Host ("`e[{0}m  InlinePredictionColor       - {0}{1}" -f $ErrorEscSeq, $DefaultEscSeq)
 
 $KeywordEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.KeywordColor
-Write-Host ("`e[{0}m  KeywordColor            - {0}{1}" -f $KeywordEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  KeywordColor                - {0}{1}" -f $KeywordEscSeq, $DefaultEscSeq)
+
+$ErrorEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.ListPredictionColor
+Write-Host ("`e[{0}m  ListPredictionColor         - {0}{1}" -f $ErrorEscSeq, $DefaultEscSeq)
+
+$ErrorEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.ListPredictionSelectedColor
+Write-Host ("`e[{0}m  ListPredictionSelectedColor - {0}{1}" -f $ErrorEscSeq, $DefaultEscSeq)
 
 $MemberEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.MemberColor
-Write-Host ("`e[{0}m  MemberColor             - {0}{1}" -f $MemberEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  MemberColor                 - {0}{1}" -f $MemberEscSeq, $DefaultEscSeq)
 
 $NumberEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.NumberColor
-Write-Host ("`e[{0}m  NumberColor             - {0}{1}" -f $NumberEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  NumberColor                 - {0}{1}" -f $NumberEscSeq, $DefaultEscSeq)
 
 $OperatorEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.OperatorColor
-Write-Host ("`e[{0}m  OperatorColor           - {0}{1}" -f $OperatorEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  OperatorColor               - {0}{1}" -f $OperatorEscSeq, $DefaultEscSeq)
 
 $ParameterEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.ParameterColor
-Write-Host ("`e[{0}m  ParameterColor          - {0}{1}" -f $ParameterEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  ParameterColor              - {0}{1}" -f $ParameterEscSeq, $DefaultEscSeq)
 
 $SelectionEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.SelectionColor
-Write-Host ("`e[{0}m  SelectionColor          - {0}{1}" -f $SelectionEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  SelectionColor              - {0}{1}" -f $SelectionEscSeq, $DefaultEscSeq)
 
 $StringEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.StringColor
-Write-Host ("`e[{0}m  StringColor             - {0}{1}" -f $StringEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  StringColor                 - {0}{1}" -f $StringEscSeq, $DefaultEscSeq)
 
 $TypeEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.TypeColor
-Write-Host ("`e[{0}m  TypeColor               - {0}{1}" -f $TypeEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  TypeColor                   - {0}{1}" -f $TypeEscSeq, $DefaultEscSeq)
 
 $VariableEscSeq = Extract-EscSeq $PSConsoleReadLineOptions.VariableColor
-Write-Host ("`e[{0}m  VariableColor           - {0}{1}" -f $VariableEscSeq, $DefaultEscSeq)
+Write-Host ("`e[{0}m  VariableColor               - {0}{1}" -f $VariableEscSeq, $DefaultEscSeq)
 
 #===================================================================================================
